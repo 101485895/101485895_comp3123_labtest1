9fb2d8ae299c1fa8f798d8398594ecb597ac70b7
101485895 <101485895@georgebrown.ca>
2025-10-02 18:43:54 -0400

    finished question 2

---
+++ question2/callbackPromises.js
@@ -0,0 +1,19 @@
+function resolvedPromise() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve({ message: "delayed success!"});
+        }, 500);
+    });
+}
+
+resolvedPromise().then(result => console.log(result)).catch(error => console.error(error))
+
+function rejectedPromise() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            reject(new Error("error: delayed exception!"));
+        }, 500);
+    });
+}
+
+rejectedPromise().then(result => console.log(result)).catch(error => console.error(error))
